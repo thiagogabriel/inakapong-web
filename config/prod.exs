aee9c6d52bbda0bc2156b519395f8ff74f007de8
Marcio Junior <marciojunior_eu@yahoo.com.br>
2017-08-04 11:33:32 -0300

    typo

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,7 @@ config :inaka_pong, InakaPongWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "nameless-castle-90436.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 
